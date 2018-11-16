40d70b99433e1f55aedaec584f566be4b7273c75
jess compacion <jagcompacion@gmail.com>
2018-11-16 16:04:40 +0800

    added initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sitebuilder library components
+
+## Installation
+```js
+yarn add https://github.com/Proto-Garage/sitebuilder.library.components.git
+```
+
+##Usage
+```js
+import { Heading } from 'sitebuilder.library.components';
+
+render() {
+  <Header />
+}
+```
+
---
+++ src/Heading.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import renderHTML from "react-render-html";
+
+interface IProps {
+  tag?: string;
+  value: string;
+}
+
+const Heading: React.SFC<IProps> = props => {
+  return <>{renderHTML(`<${props.tag}>${props.value}</${props.tag}>`)}</>;
+};
+
+Heading.defaultProps = {
+  tag: "h1"
+};
+
+export default Heading;
